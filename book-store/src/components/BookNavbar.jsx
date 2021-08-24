2268277f65f636350ca900f0917aef10d368499f
Emmanuel Iyere <40471024+emmanuelky@users.noreply.github.com>
2021-08-24 15:20:23 +0200

    added navbar

---
+++ book-store/src/components/BookNavbar.jsx
@@ -0,0 +1,32 @@
+import { Component } from 'react';
+import { Container, Navbar, Nav } from 'react-bootstrap';
+
+class BookNavbar extends Component {
+
+    render() {
+
+        return (
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Home</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#features">Browse</Nav.Link>
+                            {/* <Nav.Link href="#pricing">Pricing</Nav.Link> */}
+
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">About</Nav.Link>
+                            {/* <Nav.Link eventKey={2} href="#memes">
+                                Dank memes
+                            </Nav.Link> */}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        )
+    }
+}
+
+export default BookNavbar;
