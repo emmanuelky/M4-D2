700db38a59098e90fb817d8bfd635a8b6283f916
Emmanuel Iyere <40471024+emmanuelky@users.noreply.github.com>
2021-09-06 05:48:55 +0200

    Update

--- book-store/src/components/Welcome.jsx
+++ book-store/src/components/Welcome.jsx
@@ -3,7 +3,7 @@ const Welcome = () => {
         <div className="jumbotron jumbotron-fluid mt-4">
             <div className="container">
                 <h1 className="display-4">Welcome To Emmy's Book Shop</h1>
-                <p className="lead">Knowledge is Power. Book is Knowledge!</p>
+                <p className="lead">Book is Knowledge. Knowledge is Power!</p>
             </div>
         </div>
     )
